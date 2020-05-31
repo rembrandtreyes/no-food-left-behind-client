0910a0b35b80b596233a67f4cfd1f1b53ec4ef7f
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-05-31 10:53:35 -0700

    Add global colors

---
+++ src/config/color.js
@@ -0,0 +1,9 @@
+const colors = {
+  dark: "#313C40", // RGB 49, 60, 64
+  light: "#F2F2F2", // RGB 242, 242, 242
+  brown: "#A65E44", // RGB 166, 94, 68
+  red1: "#F26A4B", // RGB 242, 106, 75
+  red2: "#Bf5841", // RGB 191, 88, 65
+}
+
+export default colors
