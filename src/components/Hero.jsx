be71c66e7ef8f0ce19cc5757d760d6a693a8a67c
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-05-30 18:57:18 -0700

    Get Hero up and running

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React from "react"
+import styled from "styled-components"
 
 const Hero = (props) => {
   return (
-    <div>
-      <h1>Hero renders</h1>
-    </div>
-  );
-};
+    <HeroContainer>
+      <OverlayContainer>
+        <h1>No Food Left Behind</h1>
+        <button>Learn more about contributing to 0 waste</button>
+      </OverlayContainer>
+    </HeroContainer>
+  )
+}
 
-export default Hero;
+export const OverlayContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 700px;
+  height: 110%;
+  width: 50vw;
+  text-align: left;
+  align-items: flex-start;
+  margin-top: 0px;
+  padding: 16px 60px;
+  background: rgba(255, 255, 255, 0.75);
+`
+
+export const HeroContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 800px;
+  background: url("https://res.cloudinary.com/dfxj5hpgc/image/upload/v1590886651/diego-passadori-3ylL-Qqv_tY-unsplash_clf68u.jpg");
+  background-position: center center;
+  background-size: cover;
+  justify-content: flex-end;
+  margin-bottom: 125px;
+`
+
+export default Hero
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react"
+import styled from "styled-components"
+import Hero from "../components/Hero"
 
 const HomePage = (props) => {
   return (
     <div className="homepage-page">
+    <Hero />
       <h1>Homepage renders!</h1>
       <div className="homepage-description">DESCRIPTION STUFF</div>
       <div className="homepage-references">REFERENCES</div>
     </div>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default HomePage
